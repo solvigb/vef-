aa5e2960cefc113d817c7e5f4ef4f4378caf3be1
solvigb <42898098+solvigb@users.noreply.github.com>
2019-01-14 14:54:56 +0000

    Add files via upload

---
+++ verkefni1.py
@@ -0,0 +1,76 @@
+#Sölvi Geir Björnsson
+#Skilaverkefni 1
+#11.1.2019
+
+
+#                                 Liður 1
+
+
+#Breyta fyrir áfanga nafn
+afangi = "FORR1FG05AU"
+#Breyta fyrri notanda nafn
+notandi = "Sölvi"
+
+print("Velkominn í áfangann",afangi)
+
+print("þetta verður skemmtileg önn hjá okkur",notandi)
+
+
+#                                 Liður 2
+
+
+#Spyr notanda umm að slá inn tölu hærri en 100 og deili hana með 5.5
+tala = float(input("Sláðu inn töli hærri en 100"))
+#Deili með 5.5
+tala = tala1/5.5
+#Stilli á bara tvær kommutölur
+tala = (round(tala, 2))
+print("Talan sem að þú sláðir inn deilt með 5.5 er",tala)
+
+
+#                                 Liður 3
+
+
+print("Skráðu inn 2 heilar tölur til að margfalda saman")
+
+num1 = int(input("Skráðu inn heila tölu "))
+
+num2 = int(input("Skráðu inn heila tölu "))
+
+sum = num1 * num2
+
+print("Útkoma: ", sum)
+
+
+#                                 Liður 4
+
+
+print("Skráðu inn 3 heilar tölur til að finna rúmmál kassa")
+
+num3 = int(input("Skráðu inn hæð kassans "))
+
+num4 = int(input("Skráðu inn lengd kassans "))
+
+num5 = int(input("Skráðu inn breidd kassans "))
+
+sum3 = num3num4num5
+
+print("Rúmmál kassans er ", sum3)
+
+
+#                                 Liður 5
+
+ar = 2019
+aldur = int(input("Sláðu inn aldurinn þinn")
+
+
+
+
+
+
+
+
+
+
+
+
